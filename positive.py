bd50eea81c16775866f6bb311c06899b73cfbd38
poorni123 <34570385+poorni123@users.noreply.github.com>
2017-12-16 13:53:45 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,15 +1,11 @@
-n=int(raw_input())
-
-if(n>0):
-	
-   print("positive")
-
-elif(n==0):
-	
-	
-   print("zero")
-
-else:
-	
-   print("negative")
+try:
+	n=int(raw_input())
+	if(n>0):
+		print("positive")
+	elif(n==0):
+		print("zero")
+	else:
+		print("negative")
+except:
+	print('invalid input')
 	
