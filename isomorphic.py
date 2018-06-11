f337245a4ce39b2d14562c4f786cf0f8433c2faa
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-06-11 13:50:57 +0530

    Create isomorphic.py

---
+++ isomorphic.py
@@ -0,0 +1,19 @@
+# your code goes here
+# your code goes here
+a=str(input())
+b=str(input())
+c=0
+d=0
+if(len(a)==len(b)):
+ for i in range(1,len(a)):
+  if(a[i]==a[i+1]):
+   c=c+1
+  if(b[i]==b[i+1]):
+   d=d+1
+  if(c!=b):
+   print("no")
+   break
+ if(c==d):
+  print("yes")
+ else:
+  print("no")
