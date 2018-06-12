2a5a94a0de9e8e661b270e8d538253b46925b7e0
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-06-12 10:44:32 +0545

    Update prime_limit.py

--- prime_limit.py
+++ prime_limit.py
@@ -3,9 +3,9 @@ try:
 	count=0
 	n=int(raw_input())
 	m=int(raw_input())
-	for i in range (n,m):
+	for i in range (n,m+1):
 		c=0
-		for j in range (n,m):
+		for j in range (1,i+1):
 			if (i%j==0):
 				c=c+1
 			else :
