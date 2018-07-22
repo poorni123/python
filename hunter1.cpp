b91ce9e4ddd347a60b900826f9236d8c2a08d2c7
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-07-22 10:03:57 +0530

    Create hunter1.cpp

---
+++ hunter1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[100],n,i,j,c=0,t,index=0,e=0,b[100];
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+		for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+	{
+		if(a[i]==a[j])
+		{
+		
+			b[e]=a[i];
+			e++;
+		}
+	}}
+		for(i=0;i<e;i++)
+	{
+		for(j=0;j<e;j++)
+		{
+		if(b[j]>b[i] )
+		{
+			t=b[i];
+			b[i]=b[j];
+			b[j]=t;
+			
+		}
+			
+		}
+	}
+		for(i=0;i<e;i++)
+	{
+	cout<<b[i]<<" ";
+	}
+	
+	return 0;
+}
