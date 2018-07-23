0c6b53ddf1e52680e87beb2d7b44c3cb332a2bcb
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-07-23 23:40:36 +0530

    Create hunter15.cpp

---
+++ hunter15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k,i,j;
+	int a[100],sum=0;
+	cin>>n;
+		for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+		
+	
+		for(i=0;i<n;i++)
+	{
+		
+		
+		sum=sum+a[i];
+
+}
+cout<<sum;
+	return 0;
+}
