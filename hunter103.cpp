113a44fa25d084a965ea8c946c8e6e6deb956cff
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-08-01 23:02:05 +0530

    Create hunter103.cpp

---
+++ hunter103.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    int n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+    	for(int j=0;j<=i;j++)
+    	{
+    		cout<<"1"<<"\t";
+    	}
+    	cout<<"\n";
+    }
+    
+}
