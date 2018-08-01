d5f972f8e08718d4c73ed0b100043c22551ffb77
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-08-01 23:15:17 +0530

    Create hunter104.cpp

---
+++ hunter104.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    string s;
+    int i,j,sum=0;
+    cin>>s;
+    for(i=0;i<s.length();i++)
+    {
+    	for(j=0;j<=i;j++)
+    	{
+    	sum=sum+s[i];
+    	}
+    	
+    
+    }
+    	cout<<sum;
+}
