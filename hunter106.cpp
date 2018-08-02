12c219a49961a33645d33f7c25601fa72c43e942
poorni123 <34570385+poorni123@users.noreply.github.com>
2018-08-02 19:02:38 +0530

    Create hunter106.cpp

---
+++ hunter106.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Rene Decartes once said, 'I think, therefore I am.'
+int main() {
+string s;
+string s1;
+cin>>s>>s1;
+int l1=s.length();
+int l2=s1.length();
+for(int i=0;i<l1;i++)
+{
+	cout<<s1[i]<<s[i]<<"\n";
+}
+
+	return 0;
+}
